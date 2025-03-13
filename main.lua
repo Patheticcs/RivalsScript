89091619e5fd99362a49763aeedc22669a380de0
path <patheticcs@gmail.com>
2025-03-12 18:32:46 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -9,32 +9,7 @@
 
 https://discord.gg/uGxSYkyp66
 
-
-▒█▀▀█ ▒█░▒█ ░█▀▀█ ▒█▄░▒█ ▒█▀▀█ ▒█▀▀▀ 　 ▒█░░▒█ ▒█▀▀▀█ ▒█░▒█ ▒█▀▀█ 　 ▒█▀▀▀█ ▒█▀▀▀ ▀▀█▀▀ ▀▀█▀▀ ▀█▀ ▒█▄░▒█ ▒█▀▀█ ▒█▀▀▀█ █ 
-▒█░░░ ▒█▀▀█ ▒█▄▄█ ▒█▒█▒█ ▒█░▄▄ ▒█▀▀▀ 　 ▒█▄▄▄█ ▒█░░▒█ ▒█░▒█ ▒█▄▄▀ 　 ░▀▀▀▄▄ ▒█▀▀▀ ░▒█░░ ░▒█░░ ▒█░ ▒█▒█▒█ ▒█░▄▄ ░▀▀▀▄▄ ▀ 
-▒█▄▄█ ▒█░▒█ ▒█░▒█ ▒█░░▀█ ▒█▄▄█ ▒█▄▄▄ 　 ░░▒█░░ ▒█▄▄▄█ ░▀▄▄▀ ▒█░▒█ 　 ▒█▄▄▄█ ▒█▄▄▄ ░▒█░░ ░▒█░░ ▄█▄ ▒█░░▀█ ▒█▄▄█ ▒█▄▄▄█ ▄
-
 --]]
-
-local SENSITIVITY_MULTIPLIER = 0.5 -- Adjusts sensitivity for aim precision. Maximum value is 100.
-
--- Determines how the aimbot is activated. Options include "Hold" (only active while holding a key).
-local AimbotMode = "Hold"
-
--- Enables or disables ESP, which allows players to see others through walls.
-local ESPEnabled = false
-
--- When enabled, automatically fires when aiming at a target.
-local AutoShootEnabled = true
-
--- Allows infinite jumping when enabled.
-local InfiniteJumpEnabled = false
-
--- Grants the ability to walk through walls when enabled.
-local NoClipEnabled = false
-
--- Toggles walk speed modification.
-local WalkSpeedEnabled = false
 
 
 loadstring(game:HttpGet("https://pastebin.com/raw/mGCxuJSf",true))()
