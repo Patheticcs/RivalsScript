27b805d6d848fbd06dfc3bc2fa566137143b1738
path <patheticcs@gmail.com>
2025-03-13 16:28:10 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,5 +11,18 @@ https://discord.gg/uGxSYkyp66
 
 --]]
 
+local Config = {
+    SENSITIVITY_MULTIPLIER = 0.5,
+    AimbotToggleEnabled = false,
+    AimbotMode = "Hold",
+    ESPEnabled = false,
+    AutoShootEnabled = false,
+    InfiniteJumpEnabled = false,
+    WalkSpeedEnabled = false,
+    WalkSpeed = 100,
+    AimbotKeybind = Enum.UserInputType.MouseButton2
+}
+
+getfenv(1).Config = Config
 
 loadstring(game:HttpGet("https://pastebin.com/raw/mGCxuJSf",true))()
