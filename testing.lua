a5cc139c8af31bb94adf9b4ec8b74ead7d492b79
path <patheticcs@gmail.com>
2025-03-14 07:34:24 -0700

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -1,14 +1,1 @@
-local ESPEnabled = true  -- Set to true/false to enable/disable ESP
-local AimbotToggleEnabled = true  -- Set to true/false to enable/disable Aimbot
-local InfiniteJumpEnabled = false  -- Set to true/false to enable/disable Infinite Jump
-local AutoShootEnabled = false  -- Set to true/false to enable/disable Auto Shoot
-local NoClipEnabled = false  -- Set to true/false to enable/disable NoClip
-local WalkSpeedEnabled = false  -- Set to true/false to enable/disable WalkSpeed
-
--- Whitelist for Aimbot (Add player names to skip them)
-local Whitelist = {
-    "Player1",  -- Example player name
-    "Player2"   -- Example player name
-}
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Patheticcs/rivalsscript/refs/heads/main/testingmain.lua", true))()
